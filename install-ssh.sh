e0308b68953f05c3438b84837e69bb955109e507
Max Shlain <maxshlain@gmail.com>
2022-03-21 13:13:56 +0200

    Update install-ssh.sh

--- install-ssh.sh
+++ install-ssh.sh
@@ -1,5 +1,5 @@
 sudo apt-get update \
-&& sudo apt-get install openssh-server \
+&& sudo apt-get install openssh-server -y \
 && sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config \
 && sudo systemctl restart sshd \
 && sudo systemctl enable ssh
